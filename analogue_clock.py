851e5b5047e5ac9faeb4123e14dc87f4a0e15cfe
eBLDR <ed.bldr@gmail.com>
2019-06-04 19:19:34 +0200

    Refactor

--- analogue_clock.py
+++ analogue_clock.py
@@ -1,85 +1,106 @@
 import turtle
-import time
 from datetime import datetime
 
-
-def draw_minute_mark(turt, distance, angle):
-    # Minute marks
-    turt.penup()
-    turt.home()
-    turt.setheading(angle)
-    turt.forward(distance)
-    turt.pendown()
-    turt.forward(10)
+SCREEN_SIZE = 600
+CLOCK_SIZE = SCREEN_SIZE * (2 / 3)
 
 
-def draw_hour_mark(turt, distance, angle):
-    # Hour marks
-    draw_minute_mark(turt, distance, angle)
-    turt.penup()
-    turt.forward(15)
-    turt.stamp()
-
-
-def draw_clock(turt, distance):
+def draw_clock(cursor):
     # Draw full clock sphere w/ marks
-    angle = 0
-    while angle < 360:
-        draw_minute_mark(turt, distance, angle)
-        angle += 360 / 60
-    angle_h = 0
-    while angle_h < 360:
-        draw_hour_mark(turt, distance, angle_h)
-        angle_h += 360 / 12
+    radius = CLOCK_SIZE / 2
+
+    cursor.penup()
+    cursor.goto(0, - radius)
+    cursor.setheading(0)
+    cursor.pendown()
+    cursor.circle(radius)
+
+    for angle in range(0, 360, 6):
+        if angle % 30 == 0:
+            draw_time_mark(cursor, radius, angle, hour=True)
+        else:
+            draw_time_mark(cursor, radius, angle)
 
 
-def draw_hands(turt, thickness, length, angle):
-    # Draw clock's hand
-    turt.penup()
-    turt.home()
-    turt.left(angle)
-    turt.pendown()
-    turt.pensize(thickness)
-    turt.forward(length)
-    turt.penup()
+def draw_time_mark(cursor, distance, angle, hour=False):
+    cursor.penup()
+    cursor.goto(0, 0)
+    cursor.setheading(angle)
+    cursor.forward(distance)
+    cursor.pendown()
+    cursor.backward(25 if hour else 10)
 
 
-# Setting the window
-window = turtle.Screen()
-window.setup(600, 600)
-window.bgcolor('black')
-window.title('Analogue Clock by eBLDR')
+def draw_hand(cursor, length, angle):
+    cursor.clear()
+    cursor.penup()
+    cursor.goto(0, 0)
+    cursor.setheading(90)
+    cursor.right(angle)
+    cursor.pendown()
+    cursor.forward(length)
 
-# Setting turtles' objects
-atlas = turtle.Turtle()
-atlas.speed('fastest')
-atlas.shape('triangle')
-atlas.hideturtle()
-atlas.color('green')
 
-cronos = turtle.Turtle()
-cronos.speed('fastest')
-cronos.hideturtle()
-cronos.color('white')
+def draw_hand_seconds(cursor, seconds):
+    angle_second = seconds * 6
+    draw_hand(cursor, 150, angle_second)
 
-# Drawing clock
-draw_clock(atlas, 200)
 
-# Looping to draw clock's hands
-while True:
-    time_now = datetime.now()  # Getting time
-    
-    # Finding angles for hands
-    angle_hour = (time_now.hour * (-30) + 90) + time_now.minute / (-2)
-    angle_minute = time_now.minute * (-6) + 90
-    angle_second = time_now.second * (-6) + 90
-    
-    # Drawing
-    draw_hands(cronos, '10', 120, angle_hour)
-    draw_hands(cronos, '4', 180, angle_minute)
-    draw_hands(cronos, '1', 180, angle_second)
-    
-    time.sleep(0.8)  # May differ for optimal performance depending on hardware
-    cronos.clear()
-    
-    window.listen()  # It will raise a terminator error on exit
+def draw_hand_minutes(cursor, minutes):
+    angle_minute = minutes * 6
+    draw_hand(cursor, 120, angle_minute)
+
+
+def draw_hand_hours(cursor, hours, minutes):
+    angle_hour = hours * 30 + minutes / 2
+    draw_hand(cursor, 90, angle_hour)
+
+
+def cursor_generator(thickness=1):
+    # Setting turtles' objects
+    cursor = turtle.Turtle()
+    cursor.speed(0)
+    cursor.hideturtle()
+    cursor.color('white')
+    cursor.pensize(thickness)
+
+    return cursor
+
+
+def main():
+    # Setting the window
+    window = turtle.Screen()
+    window.setup(SCREEN_SIZE, SCREEN_SIZE)
+    window.bgcolor('black')
+    window.title('Analogue Clock by eBLDR')
+
+    atlas = cursor_generator()
+
+    cronus_hour = cursor_generator(thickness=5)
+    cronus_minute = cursor_generator(thickness=3)
+    cronus_second = cursor_generator()
+
+    # Init
+    draw_clock(atlas)
+
+    past = datetime.now()
+
+    draw_hand_seconds(cronus_second, past.second)
+    draw_hand_minutes(cronus_minute, past.minute)
+    draw_hand_hours(cronus_hour, past.hour, past.minute)
+
+    # Looping to draw clock's hands
+    while True:
+        now = datetime.now()
+
+        if now.second != past.second:
+            draw_hand_seconds(cronus_second, now.second)
+
+            if now.minute != past.minute:
+                draw_hand_minutes(cronus_minute, now.minute)
+                draw_hand_hours(cronus_hour, now.hour, now.minute)
+
+            past = now
+
+
+main()
