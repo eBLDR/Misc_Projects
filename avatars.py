46d5d8f03c9d676dc0201f12d9a4841c6d05ff89
eBLDR <ed.bldr@gmail.com>
2019-07-31 15:33:40 +0200

    Moved to another project

--- avatars.py
+++ avatars.py
@@ -11,16 +11,16 @@ class Avatar:
         self.save_file = save_file
         self.array = Array(size=size, symmetric=symmetric)
         self.image = Canvas(unit_size=size)
-    
+
     def generate(self):
         self.array.generate_array()
         self.image.fill_canvas(self.array.values)
         if self.save_file:
             self.save()
-    
+
     def display(self):
         self.image.canvas.show()
-    
+
     def save(self):
         self.image.canvas.save('avatar_{}.png'.format(str(uuid.uuid4())), 'PNG')
 
@@ -32,7 +32,7 @@ class Array:
         self.symmetric = kwargs.get('symmetric', False)
         if self.symmetric:
             assert self.size % 2 != 0, 'Only odd numbers for symmetric mode - it looks cooler.'
-    
+
     def generate_array(self):
         white_color = (255, 255, 255)
         color = self.get_random_rgb_color()
@@ -52,13 +52,13 @@ class Array:
             self.values = array
         else:
             self.values = [[color if random.randint(0, 1) == 1 else white_color for c in range(self.size)] for r in range(self.size)]
-    
+
     def get_random_rgb_color(self):
         color = tuple()
         for i in range(3):
             color += (self.get_random_value(),)
         return color
-    
+
     @staticmethod
     def get_random_value(max_=255):
         return random.randint(0, max_)
@@ -69,7 +69,7 @@ class Canvas:
         self.pixels_per_unit_size = 50
         self.pixels = self.pixels_per_unit_size * unit_size
         self.canvas = Image.new('RGB', (self.pixels, self.pixels))
-    
+
     def fill_canvas(self, array_of_rgb_values):
         for i in range(len(array_of_rgb_values)):
             for j in range(len(array_of_rgb_values[i])):
@@ -79,6 +79,6 @@ class Canvas:
 
 
 if __name__ == '__main__':
-    avatar = Avatar(symmetric=True, size=5, save_file=False)
+    avatar = Avatar(symmetric=True, size=9, save_file=False)
     avatar.generate()
     avatar.display()
