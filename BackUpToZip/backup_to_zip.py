5319283b93ab78cd03cb7d597909e48185485b25
eBLDR <ed.bldr@gmail.com>
2019-08-30 12:33:17 +0200

    Removed absolute path

--- BackUpToZip/backup_to_zip.py
+++ BackUpToZip/backup_to_zip.py
@@ -6,43 +6,56 @@ import os
 import zipfile
 
 
-def backup_to_zip(folder):
-    # Backup the entire contents of "folder" into a ZIP file.
-    folder = os.path.abspath(folder)  # Finding absolute path, in case folder was relative
-    
+def backup_to_zip(src_folder):
+    # Backup the entire contents of specified folder into a ZIP file.
+    src_folder = os.path.abspath(src_folder)  # Finding absolute path, in case folder was relative
+
     # Figure out the filename based on what files already exist
     number = 1
+
     while True:
         # Generating the filename
-        zip_filename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        zip_filename = os.path.basename(src_folder) + '_' + str(number) + '.zip'
+
         # Checking if exists
         if not os.path.exists(zip_filename):
             break
+
         number += 1
-    
+
     # Create the ZIP file
-    print('Creating {} . . .'.format(zip_filename))
-    
+    print('Creating {} ...'.format(zip_filename))
+
+    # Get current working directory
+    cwd = os.getcwd()
+
     with zipfile.ZipFile(zip_filename, 'w') as backup_zip:
-        
+
         # Walk the entire folder tree and compress the files in each folder
-        for folder_name, sub_folders, file_names in os.walk(folder):
-            print('Adding files in {} . . .'.format(folder_name))
-            
+        for folder_name, sub_folders, file_names in os.walk(src_folder):
+
+            # Save folder into archive without absolute path
+            target_folder = folder_name.replace(cwd, '')
+
+            print('Adding files to {} ...'.format(target_folder))
+
             # Add the current folder to the ZIP file
-            backup_zip.write(folder_name)
-            
+            backup_zip.write(folder_name, arcname=target_folder)
+
             # Add all the files in this folder to the ZIP file
             for filename in file_names:
-                
+
                 # To avoid backing up the backup ZIP files already existing
-                new_base = os.path.basename(folder) + '_'
+                new_base = os.path.basename(src_folder) + '_'
                 if filename.startswith(new_base) and filename.endswith('.zip'):
                     continue
-                
+
                 # Add the file
-                backup_zip.write(os.path.join(folder_name, filename))
-    
+                backup_zip.write(
+                    os.path.join(folder_name, filename),
+                    arcname=os.path.join(target_folder, filename)
+                )
+
     print('Done.')
 
 
